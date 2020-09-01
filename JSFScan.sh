db1dae0cff422f5a6f047015fe3bf16506ece7da
Kathan Patel <patelkathan22@gmail.com>
2020-09-01 17:52:43 +0530

    Fix and issue with httpx.
    Fixed the issue that cause live_jsfiles_lnk.txt to be empty by fixing the command for httpx.

--- JSFScan.sh
+++ JSFScan.sh
@@ -21,7 +21,7 @@ echo -e "\n\e[36m[\e[32m+\e[36m]\e[92m Started Gathering JsFiles-links\e[0m\n";
 cat $target | gau | grep -iE "\.js$" | uniq | sort >> jsfile_links.txt
 cat $target | subjs >> jsfile_links.txt
 cat $target | hakrawler -js -depth 2 -scope subs -plain >> jsfile_links.txt
-cat jsfile_links.txt | httpx -silent -follow-redirects -status-code | grep 200 | awk '{print $1}' | sort -u > live_jsfile_links.txt
+cat jsfile_links.txt | httpx -silent -status-code | grep "[200]" | cut -d ' ' -f1 | sort -u  > live_jsfile_links.txt
 }
 
 #Gather Endpoints From JsFiles
